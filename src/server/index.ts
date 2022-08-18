9557ccfa5fef6f30b40b7f0d3a8fe303efbe352d
Rinat <lich666black@gmail.com>
2022-08-18 15:08:32 +0300

    fixed request

--- src/server/index.ts
+++ src/server/index.ts
@@ -12,25 +12,43 @@ fastify.get<{
   Querystring: {
     limit: number;
     offset: number;
+    action: number;
+    rarity: "DESC" | 'ASC';
+    strong: "DESC" | 'ASC';
   };
   Params: {
     address: string;
   };
 }>('/api/v1/tokens/:address', async (request, reply) => {
-  const { limit = 100, offset = 0 } = request.query;
+  const {
+    action,
+    limit = 100,
+    offset = 0,
+    rarity = 'DESC',
+    strong = 'DESC'
+  } = request.query;
   const address = request.params.address;
   const tokenRepo = orm.getRepository(Token);
 
   try {
-    const tokens = await tokenRepo.findAndCount({
+    const [tokens, count] = await tokenRepo.findAndCount({
       where: {
-        owner: address
+        action,
+        owner: address,
+        burnt: false
+      },
+      order: {
+        rarity,
+        strong
       },
       take: limit,
       skip: offset
     });
   
-    reply.code(200).send(tokens);
+    reply.code(200).send({
+      tokens,
+      count
+    });
   } catch (err) {
     console.log(err);
     reply.code(500).send((err as Error).message);
